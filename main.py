1d7ed85bd4110e73ed4f050f27403f3a9918d799
pliduino <liduino@usp.br>
2022-05-27 00:11:06 -0300

    Added procedural colors, scale + matrices

--- main.py
+++ main.py
@@ -4,54 +4,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+#Calculates the magnitude of 2D Vectors
 def magnitude(x, y):
     return np.sqrt(x**2 + y**2)
 
+
 #Plot Functions
 def test_func(x, y):
-    if(x == 0 and y ==0):
+    if(x == 0 and y == 0):
         return 0
     return -y/(x**2 +y**2)
-    #return x*y
+    # return x*y
 
 def test_func2(x, y):
-    if(x == 0 and y ==0):
+    if(x == 0 and y == 0):
         return 0
     return x/(x**2 +y**2)
-    #return x**2
+    # return x**2
 
 func_x = np.vectorize(test_func)
 func_y = np.vectorize(test_func2)
 
-
 #Values to compute
-range = 2.5
-step = 0.5
+range = 5
+step = 0.25
 size = np.arange(-range, range + step, step)
 
 #Creating subplot to later set spines and colors
 fig, ax = plt.subplots()
 
 
-#!Setting Colormap
-min_color = 0
-max_color = 5
+#Calculating Vectors
+points = np.array(np.meshgrid(size, size))
+vec_x = func_x(points[0], points[1])
+vec_y = func_y(points[0], points[1])
+vmagnitude = np.vectorize(magnitude)
+magnitude_values = vmagnitude(vec_x, vec_y)
 
-cmap = mpl.cm.get_cmap('plasma', 50000)
+#Setting Colormap
+min_color = np.min(magnitude_values)
+max_color = np.max(magnitude_values)
+cmap = mpl.cm.get_cmap('plasma', 500)
 norm = mpl.colors.Normalize(min_color, max_color)
 
 
-#PLotting Vectors
-
-points = np.array(np.meshgrid(size, size))
-print(points)
-
-for x in size:
-    for y in size:
-        vec_x = func_x(x, y)
-        vec_y = func_y(x, y)
-        norm_mag = norm(magnitude(vec_x, vec_y))
-        ax.quiver(x, y, vec_x, vec_y, units='xy',scale_units='xy', scale=5, color=cmap(norm_mag))
+#Plotting Vectors
+ax.quiver(points[0], points[1], vec_x, vec_y, norm(magnitude_values), units='xy', scale_units='xy', scale=max_color/step)
 
 
 #Setting Spines
@@ -60,7 +58,7 @@ ax.spines['bottom'].set_position('zero')
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
-
+#Setting Colorbar
 fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax)
 
 
